223c20a3eb03e87f4c8e4db6f1fb78ec55ce69d3
CS student <me@DebianXfce23F.cs.uoregon.edu>
2026-09-10 09:15:06 -0700

    set up basic environment

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+import Gold_Display from './Gold_Display';
+
+function App() {
+
+  return (
+    <h1><Gold_Display></Gold_Display></h1>
+  )
+}
+
+export default App;
---
+++ frontend/src/Gold_Display.tsx
@@ -0,0 +1,8 @@
+
+
+function Gold_Display(){
+    let value: number = 69421;
+    return <p>{value}</p>;
+}
+
+export default Gold_Display;
